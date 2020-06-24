89c1f548f056fa58fc708aaa7011ce82a4114b48
Oluwaseun <oluwaseunazure@yahoo.com>
2020-06-24 07:15:09 +0100

    implicit route binding

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Article;
 
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{article}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{article}', 'ArticleController@update');
+Route::delete('articles/{article}', 'ArticleController@delete');
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
