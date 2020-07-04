d6f19c25dc8ea4edf743768e045a0e5132694ab9
Oluwaseun <oluwaseunazure@yahoo.com>
2020-07-04 16:48:58 +0100

    phpmailer

--- api/create_user.php
+++ api/create_user.php
@@ -1,4 +1,12 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+//require 'C:\wamp64\composer\vendor\autoload.php';
+require 'C:\wamp64\composer\vendor\phpmailer\phpmailer\src\Exception.php';
+require 'C:\wamp64\composer\vendor\phpmailer\phpmailer\src\PHPMailer.php';
+require 'C:\wamp64\composer\vendor\phpmailer\phpmailer\src\SMTP.php';
+
 // required headers
 header("Access-Control-Allow-Origin: http://localhost:8080/rest-api-authentication/");
 header("Content-Type: application/json; charset=UTF-8");
@@ -9,6 +17,8 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 // files needed to connect to database
 include_once 'config/database.php';
 include_once 'objects/user.php';
+
+$mail = new PHPMailer();
  
 // get database connection
 $database = new Database();
@@ -41,21 +51,73 @@ if( !$email_exists &&
     // set response code
     http_response_code(200);
 
+    try{
+
+    
     $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"."activate.php?email=". $data->email ;
     $toEmail = $data->email;
     $subject = "User Registration Activation Email";
     $content = "Click this link to activate your account. <a href='" . $actual_link . "'>" . $actual_link . "</a>";
     $mailHeaders = "From: Admin\r\n";
-    if(mail($toEmail, $subject, $content, $mailHeaders)) {
-        $message = "You have registered and the activation mail is sent to your email. Click the activation link to activate you account, thank you.";	
-        $type = "success";
-    }
-    else {
-        $message = "Problem in registration. Try Again!";	
-    }
+    // if(mail($toEmail, $subject, $content, $mailHeaders)) {
+    //     $message = "You have registered and the activation mail is sent to your email. Click the activation link to activate you account, thank you.";	
+    //     $type = "success";
+    // }
+    // else {
+    //     $message = "Problem in registration. Try Again!";	
+    // }
  
     // display message: user was created
     echo json_encode(array("message" => "User was created."));
+
+    /* Open the try/catch block. */
+
+    /* Set the mail sender. */
+    $mail->setFrom('pitasonandsmartpro@gmail.com', 'Logistics Admin');
+ 
+    /* Add a recipient. */
+    $mail->addAddress($data->email, $data->firstname);
+ 
+    /* Set the subject. */
+    $mail->Subject = 'Welcome';
+ 
+    /* Set the mail message body. */
+    $mail->Body = "Click this link to activate your account. <a href='" . $actual_link . "'>" . $actual_link . "</a>";
+        
+
+    /* SMTP parameters. */
+   
+   /* Tells PHPMailer to use SMTP. */
+   $mail->isSMTP();
+   
+   /* SMTP server address. */
+   $mail->Host = 'smtp.gmail.com';
+
+   /* Use SMTP authentication. */
+   $mail->SMTPAuth = TRUE;
+   
+   /* Set the encryption system. */
+   $mail->SMTPSecure = 'tls';
+   
+   /* SMTP authentication username. */
+   $mail->Username = 'pitasonandsmartpro@gmail.com';
+   
+   /* SMTP authentication password. */
+   $mail->Password = 'p&s@12345678';
+   
+   /* Set the SMTP port. */
+   $mail->Port = 587;
+    /* Finally send the mail. */
+    $mail->send();
+    }
+    catch (Exception $e)
+{
+   echo $e->errorMessage();
+}
+catch (\Exception $e)
+{
+   echo $e->getMessage();
+}
             
 }
  
